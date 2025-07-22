43ee37b04bedf962297ca398af1b75a8d486c433
Akanksha Patil <akankshapatil.sits.it@gmail.com>
2025-07-22 12:59:23 +0530

    update commit

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
 import os
+import pickle
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from flask import Flask
+from flask import Flask, jsonify
 import models.customers
 import models.product
 import models.transaction
+
 from routes.customer_routes import router as customer_routes
 from routes.product_routes import router as product_routes
 from routes.transaction_routes import router as transaction_routes
@@ -25,6 +28,25 @@ app.register_blueprint(transaction_routes)
 @app.route('/')
 def home():
     return "shopsmart API is working"
+
+location_recommendation_path = os.path.join('ML_model/location_recommendation.pkl')
+
+with open(location_recommendation_path, 'rb') as f:
+    location_recommendations = pickle.load(f)
+
+@app.route('/recommend/location/<string:location>', methods=['GET'])
+def recommend_for_location(location):
+    location = location.lower()
+    available_locations = {loc.lower(): loc for loc in location_recommendations}
+
+    if location not in available_locations:
+        return jsonify({"error": "location not found"}), 404
+
+    original_key = available_locations[location]
+    return jsonify({
+        "location": original_key,
+        "top_products": location_recommendations[original_key]
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
