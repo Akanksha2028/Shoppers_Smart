d8fcf7fe4733abe50ed2972e7424a074a04cea87
Akanksha2028 <akankshapatil.sits.it@gmail.com>
2025-11-26 17:22:21 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,17 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:2820@localhost/shopsmart_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Try to read DB_URI from environment (Render)
+db_uri = os.getenv("DB_URI")
+
+# If not set (or on Render where MySQL is not available), use SQLite file
+if not db_uri:
+    db_uri = "sqlite:///shopsmart.db"   # file-based DB inside project
+
+app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
